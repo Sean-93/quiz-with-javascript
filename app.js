67f8e438b2630763a8e492cdab5a0b1d77021505
Sean Rooney <seandanielrooney@gmail.com>
2020-02-09 13:23:51 -0700

    added HTML + CSS files as well as linked to CSS and Javascript files as well as to Bootstrap

---
+++ app.js
@@ -0,0 +1,9 @@
+//question structure to build off
+
+question: "is the sky blue?"
+answers: [
+    { text: "yes", isCorrect: true },
+    { text: "no", isCorrect: false},
+    { text: "maybe", isCorrect: false},
+    
+];
